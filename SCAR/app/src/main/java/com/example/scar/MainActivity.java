725cb30b9a242fd681fa9707df0e093dbe23f207
Akubaxi <c.0.d.3.s.kw@gmail.com>
2024-10-29 20:45:02 -0300

    SCAR

---
+++ SCAR/app/src/main/java/com/example/scar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.scar;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Configura a Toolbar
+        Toolbar toolbar = findViewById(R.id.menu);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        return super.onOptionsItemSelected(item);
+
+}
+
+
+    }
+
+
